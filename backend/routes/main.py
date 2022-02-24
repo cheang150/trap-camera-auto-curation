b7a1a91dff6cec1525d959d6ab5859075eb1cd21
Wai Bin Cheang <cheang150@MacBook.realtek>
2022-02-24 18:01:42 +0800

    Added dummy py script, added error logging for process spawn

---
+++ backend/routes/main.py
@@ -0,0 +1,8 @@
+import sys
+
+test = sys.argv[1]
+# sys.stdout.flush()
+print(test)
+
+for i in range(10):
+    print("hello world")
--- backend/routes/python.js
+++ backend/routes/python.js
@@ -2,35 +2,36 @@ var express = require("express");
 var router = express.Router();
 
 router.post("/", function (req, res, next) {
-  const videoData = req.body;
+  var videoData = req.body;
   const videoFile = req.files.object;
   videoFile.mv(`${__dirname}/../public/${videoFile.name}`, function (err) {
     if (err) {
       return res.status(500).send(err);
     }
     videoData.path = `${__dirname}/../public/${videoFile.name}`;
-    res.send(videoData);
+    videoData = JSON.stringify(videoData);
+    console.log(videoData);
+
+    try {
+      var spawn = require("child_process").spawn;
+      var process = spawn("python", [`${__dirname}/main.py`, videoData]);
+
+      process.on("error", function (err) {
+        console.log(err);
+      });
+
+      process.stdout.on("data", function (data) {
+        console.log(data.toString());
+      });
+
+      process.on("close", (code) => {
+        console.log(`process quit with ${code}`);
+        res.send(`completed`);
+      });
+    } catch (err) {
+      console.log("spawn error:" + err);
+    }
   });
-
-  //   var spawn = require("child_process").spawn;
-  //   var process = spawn("python", [
-  //     "../detection/main.py",
-  //     req.query.video_file,
-  //     req.query.start_time,
-  //     req.query.end_time,
-  //     req.query.superResponse,
-  //     req.query.histResponse,
-  //     req.query.autoResponse,
-  //   ]);
-
-  //   process.stdout.on("data", function (data) {
-  //     console.log(data.toString());
-  //   });
-
-  //   process.on("close", (code) => {
-  //     console.log(`process quit with ${code}`);
-  //     res.send(`completed`);
-  //   });
 });
 
 module.exports = router;
