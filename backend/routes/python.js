768c98295807e4f6914210b7d650e538a6d1213f
Wai Bin Cheang <cheang150@MacBook.local>
2022-03-01 21:11:55 +0800

    Disable dummy logging response

--- backend/routes/python.js
+++ backend/routes/python.js
@@ -1,4 +1,3 @@
-const { response } = require("express");
 var express = require("express");
 var fs = require("fs");
 var router = express.Router();
@@ -24,50 +23,50 @@ router.post("/", function (req, res, next) {
     }
     videoData.path = `${__dirname}/../public/${videoFile.name}`;
 
-    res.send([
-      "Moviepy - Running:\r\n" +
-        '>>> "+ " ".join(cmd)\r\n' +
-        "Moviepy - Command successful\r\n" +
-        "Images Captured: 62\r\n" +
-        "\r\n" +
-        "Images Shortlisted: 6\r\n" +
-        "\r\n" +
-        "Background Subtraction results path: /Users/cheang150/Desktop/Trap-Camera-Auto-Curation/trap-camera-auto-curation/backend/results/Background/high.mp4\r\n" +
-        "\r\n" +
-        "Images Created: Hard-coded 88\r\n" +
-        "\r\n" +
-        "Mega results path: /Users/cheang150/Desktop/Trap-Camera-Auto-Curation/trap-camera-auto-curation/backend/results/Mega/high.mp4\r\n" +
-        "\r\n",
-    ]);
-    // var logging = [];
-    // try {
-    //   var spawn = require("child_process").spawn;
-    //   var process = spawn("python", [
-    //     `script/main.py`,
-    //     videoData.path,
-    //     videoData.startTime,
-    //     videoData.endTime,
-    //     videoData.resolutionModel,
-    //     videoData.histogramEq,
-    //     videoData.autoEnhance,
-    //   ]);
+    // res.send([
+    //   "Moviepy - Running:\r\n" +
+    //     '>>> "+ " ".join(cmd)\r\n' +
+    //     "Moviepy - Command successful\r\n" +
+    //     "Images Captured: 62\r\n" +
+    //     "\r\n" +
+    //     "Images Shortlisted: 6\r\n" +
+    //     "\r\n" +
+    //     "Background Subtraction results path: /Users/cheang150/Desktop/Trap-Camera-Auto-Curation/trap-camera-auto-curation/backend/results/Background/high.mp4\r\n" +
+    //     "\r\n" +
+    //     "Images Created: Hard-coded 88\r\n" +
+    //     "\r\n" +
+    //     "Mega results path: /Users/cheang150/Desktop/Trap-Camera-Auto-Curation/trap-camera-auto-curation/backend/results/Mega/high.mp4\r\n" +
+    //     "\r\n",
+    // ]);
+    var logging = [];
+    try {
+      var spawn = require("child_process").spawn;
+      var process = spawn("python", [
+        `script/main.py`,
+        videoData.path,
+        videoData.startTime,
+        videoData.endTime,
+        videoData.resolutionModel,
+        videoData.histogramEq,
+        videoData.autoEnhance,
+      ]);
 
-    //   process.on("error", function (err) {
-    //     console.log(err);
-    //   });
+      process.on("error", function (err) {
+        console.log(err);
+      });
 
-    //   process.stdout.on("data", function (data) {
-    //     logging.push(data.toString());
-    //   });
+      process.stdout.on("data", function (data) {
+        logging.push(data.toString());
+      });
 
-    //   process.on("close", (code) => {
-    //     console.log(logging);
-    //     console.log(`process quit with ${code}`);
-    //     res.send(logging);
-    //   });
-    // } catch (err) {
-    //   console.log("spawn error:" + err);
-    // }
+      process.on("close", (code) => {
+        console.log(logging);
+        console.log(`process quit with ${code}`);
+        res.send(logging);
+      });
+    } catch (err) {
+      console.log("spawn error:" + err);
+    }
   });
 });
 
